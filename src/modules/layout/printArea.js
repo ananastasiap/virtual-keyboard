2133f34e9c3a0e239cdc0db0b059db817e2fa6e2
ananastasiap <anestheziia@gmail.com>
2023-07-22 08:12:09 +0300

    style: printArea

--- src/modules/layout/printArea.js
+++ src/modules/layout/printArea.js
@@ -4,8 +4,10 @@ export class PrintArea {
 
   createPrintArea() {
     const printArea = document.createElement('textarea');
-    printArea.classList.add('textarea');
-    document.body.appendChild(printArea);
+    printArea.classList.add('printArea');
+
+    const mainArea = document.querySelector('.main');
+    mainArea.appendChild(printArea);
     return printArea;
   }
 }
